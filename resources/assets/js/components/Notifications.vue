301e0b01e8f6aa9e8781507d634036e8e6f6d73b
Apostolos Siokas <apostolossiokas@gmail.com>
2017-03-02 13:29:08 +0200

    WIP

--- resources/assets/js/components/Notifications.vue
+++ resources/assets/js/components/Notifications.vue
@@ -2,30 +2,33 @@
         <li class="dropdown hidden-xs">
             <a href="#" class="dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="true">
                 <i class="icon-bell"></i> 
-                <span class="badge badge-xs badge-danger" v-if="unreadNotifications">
+                <span class="badge badge-xs badge-danger" v-if="unreadNotifications.length">
                     {{ unreadNotifications.length }}
                 </span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg">
-                <li class="notifi-title"><span class="label label-default pull-right">New 3</span>Notification</li>
+                <li class="notifi-title"><span class="label label-default pull-right"></span>Ειδοποιήσεις</li>
                 <li class="list-group nicescroll notification-list">
-                <a href="/panel/school/requests" class="list-group-item">
+                <a href="/panel/school/requests" class="list-group-item"  v-if="unreadNotifications.length">
                    <div class="media">
                       <div class="pull-left p-r-10">
                          <em class="fa fa-user-plus fa-2x text-info"></em>
                       </div>
-                      <div class="media-body">
+                      <div class="media-body"> 
                          <h5 class="media-heading">Ειδοποίηση για σύνδεση</h5>
                          <p class="m-0">
-                             <small v-if="unreadNotifications">{{ unreadNotifications.length }} αιτήματα</small>
+                             <small v-if="notification.type == 'something'">{{ unreadNotifications.length }} αιτήματα</small>
                          </p>
                       </div>
                    </div>
                 </a>
                 </li>
                 <li>
-                    <a href="/panel/school/requests" class="list-group-item text-right">
-                        <small class="font-600">See all notifications</small>
+                    <div class="list-group-item text-right">
+                        <small class="font-600" v-if="!unreadNotifications.length">Δεν υπάρχουν νεες ειδοποιησεις</small>
+                    </div>
+                    <a href="/panel/school/requests" class="list-group-item text-right" v-if="unreadNotifications.length">
+                        <small class="font-600">Δείτε όλες τις ειδοποιησεις</small>
                     </a>
                 </li>
             </ul>
@@ -47,34 +50,13 @@
                     .then(response => {
                         this.unreadNotifications = response.data['unread']
                     });
-
-                    this.listenOnStudentToSchoolConnection()
+                }
             },
-
-            listenOnStudentToSchoolConnection: function(){
-                var pusher = new Pusher('943717bf5769e7b902b4', {
-                  cluster: 'eu',
-                  encrypted: true
-                });
-
-                var channel = pusher.subscribe('school.' + window.Connection);
-                channel.bind('App\\Events\\StudentAppliedToSchool', function(data) {
-                  this.getNotifications();
-                }.bind(this));
-
-                channel.bind('App\\Events\\TeacherAppliedToSchool', function(data) {
-                  this.getNotifications();
-                }.bind(this));
-            }
-        },
 
         mounted() {
             console.log('Notifications Component ready.')
 
             this.getNotifications()
-            Event.$on('readNotifications', () =>
-              this.getNotifications()
-            )
         }
     }
 </script>
