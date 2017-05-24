9292c998a621bbba33f5fb5c9f963779349100e0
Apostolos Siokas <apostolossiokas@gmail.com>
2017-05-24 11:47:12 +0300

    Adding 503 error for down server

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,29 @@
+
+@extends('errors.layouts.main')
+
+@section('content')
+            <!-- Your text -->
+            <h1 class="hand">Πέσαμε</h1>
+            <div class="big-content" style="margin-top: 30px;">
+                <!-- Main squares for the content logo in the background -->
+                <div class="list-square">
+                    <span class="square"></span>
+                    <span class="square"></span>
+                    <span class="square"></span>
+                </div>
+
+                <!-- Main lines for the content logo in the background -->
+                <div class="list-line">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+                <!-- The animated searching tool -->
+                <i class="fa fa-ban" aria-hidden="true"></i>
+            </div>
+            <p>Ο σερβερ δεν ανταποκρίνεται αυτην τη στιγμη. Σε λιγακι ελα!</p>
+
+@endsection
