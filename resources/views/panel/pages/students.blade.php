ab1d963ac9dc0ddb4921bf48b6b930f89fea1662
Apostolos Siokas <apostolossiokas@gmail.com>
2018-03-14 11:07:28 +0200

    testing3

--- resources/views/panel/pages/students.blade.php
+++ resources/views/panel/pages/students.blade.php
@@ -69,7 +69,7 @@
 
 //            $(".full-content").hide().fadeIn(100);
 
-            $('#nav').text(navigator.userAgent);
+            $('#nav').text(navigator.platform);
 
             console.log(navigator)
 
