8d14d60e8135378b96b23ca6e3c73d6f4b51c479
Apostolos Siokas <apostolossiokas@gmail.com>
2018-05-17 10:45:08 +0300

    disable interests table

--- database/migrations/2018_04_13_232403_create_school_applications_table.php
+++ database/migrations/2018_04_13_232403_create_school_applications_table.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateSchoolApplicationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('school_applications', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->nullable();
-            $table->integer('study_id')->nullable();
-            $table->string('tel')->nullable();
-            $table->timestamps();
+    // /**
+    //  * Run the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function up()
+    // {
+    //     Schema::create('school_applications', function (Blueprint $table) {
+    //         $table->increments('id');
+    //         $table->integer('user_id')->nullable();
+    //         $table->integer('study_id')->nullable();
+    //         $table->string('tel')->nullable();
+    //         $table->timestamps();
 
-        });
-    }
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('school_applications');
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function down()
+    // {
+    //     Schema::dropIfExists('school_applications');
+    // }
 }
