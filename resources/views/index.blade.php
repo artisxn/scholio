dd432b42e050ae53f2e7a933272a5dc7f5284434
Apostolos Siokas <apostolossiokas@gmail.com>
2018-05-21 22:10:22 +0300

    nnn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -65,7 +65,7 @@
   src="https://code.jquery.com/jquery-2.2.4.js"
   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
   crossorigin="anonymous"></script>
-	<script async src="{{asset('/js/jquery224.js')}}"></script>
+	<!-- <script async src="{{asset('/js/jquery224.js')}}"></script> -->
 
 
 	@include('javascript_lang')
