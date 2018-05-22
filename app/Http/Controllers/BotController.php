34777c84d40c6317e56aa7f99b3bf96278b4b048
Apostolos Siokas <apostolossiokas@gmail.com>
2018-05-22 18:12:48 +0300

    bot

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AlgoliaScholarship;
+use App\Models\AlgoliaSchool;
+use App\Models\Request;
+use App\Models\School;
+
+class BotController extends Controller
+{
+    public function appSchool(School $school)
+    {
+        $levels = [];
+        foreach ($school->type->level as $level) {
+            array_push($levels, $level);
+            foreach ($level->section as $section) {
+                foreach ($section->study as $study) {
+                }
+            }
+        }
+        $data = array(
+            'levels' => $levels,
+        );
+        return [$school->load('admin', 'type', 'image'), $data];
+    }
+
+    public function appSchoolNum($num)
+    {
+        return School::with('admin', 'type')->take($num)->get();
+    }
+
+    public function appAllSchools()
+    {
+        return School::with('admin', 'type')->get();
+    }
+
+    public function appFind($find)
+    {
+        $results = AlgoliaSchool::search($find)->get();
+
+        return $results;
+
+        if ($results->isEmpty()) {
+            return [
+                "redirect_to_blocks" => ["NotFound"],
+            ];
+        }
+
+        $elements = [];
+
+        foreach ($results as $index => $result) {
+            $array = array(
+                "title" => $result->name,
+                "image_url" => 'https://schol.io' . $result->image,
+                "subtitle" => $result->lengthScholarships(),
+                "studies" => $result->lengthStudies(),
+                "buttons" => array(
+                    [
+                        "type" => "web_url",
+                        "url" => "https://schol.io/public/profile/" . $result->id,
+                        "title" => "Προβολή",
+                    ],
+                    [
+                        "type" => "web_url",
+                        "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
+                        "title" => "Υποτροφίες",
+                    ],
+                ),
+            );
+            if ($index <= 9) {
+                array_push($elements, $array);
+            }
+        }
+        return $elements;
+    }
+
+    public function schoolSearch()
+    {
+        $results = AlgoliaSchool::search(request()->search)->get();
+
+        if ($results->isEmpty()) {
+            return [
+                "redirect_to_blocks" => ["NotFound"],
+            ];
+        }
+
+        $elements = [];
+
+        foreach ($results as $index => $result) {
+            $array = array(
+                "title" => $result->name,
+                "image_url" => 'https://schol.io' . $result->image,
+                "subtitle" => $result->lengthScholarships . " Υποτροφίες",
+                "buttons" => array(
+                    [
+                        "type" => "web_url",
+                        "url" => "https://schol.io/public/profile/" . $result->id,
+                        "title" => "Προβολή",
+                    ],
+                    [
+                        "type" => "web_url",
+                        "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
+                        "title" => "Υποτροφίες",
+                    ],
+                ),
+            );
+            if ($index <= 9) {
+                array_push($elements, $array);
+            }
+        }
+
+        $json = [
+            "messages" => [
+                [
+                    "attachment" => [
+                        "type" => "template",
+                        "payload" => [
+                            "template_type" => "generic",
+                            "elements" => $elements,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        return $json;
+    }
+
+    public function scholarshipSearch()
+    {
+        $results = AlgoliaScholarship::search(request()->search)->get();
+
+        if ($results->isEmpty()) {
+            return [
+                "redirect_to_blocks" => ["NotFound"],
+            ];
+        }
+
+        $elements = [];
+
+        foreach ($results as $index => $result) {
+            $array = array(
+                "title" => $result->study,
+                "image_url" => 'https://schol.io/' . $result->school_logo,
+                // "image_url" => 'https://schol.io/panel/assets/images/steps' . $result->section . '.png',
+                "subtitle" => $result->criteria,
+                "buttons" => array(
+                    [
+                        "type" => "web_url",
+                        "url" => "https://schol.io/scholarship/" . $result->scholarship_id,
+                        "title" => "Προβολή",
+                    ],
+                    [
+                        "type" => "web_url",
+                        "url" => 'https://schol.io/public/profile/' . $result->school_id,
+                        "title" => $result->school,
+                    ],
+                ),
+            );
+            if ($index <= 9) {
+                array_push($elements, $array);
+            }
+        }
+
+        $json = [
+            "messages" => [
+                [
+                    "attachment" => [
+                        "type" => "template",
+                        "payload" => [
+                            "template_type" => "generic",
+                            "elements" => $elements,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        return $json;
+    }
+
+    public function scholarshipCustom()
+    {
+        $results = AlgoliaScholarship::search(request()->search)->get();
+
+        if ($results->isEmpty()) {
+            return [
+                "redirect_to_blocks" => ["NotFound"],
+            ];
+        }
+
+        $elements = [];
+
+        foreach ($results as $index => $result) {
+            $array = array(
+                "title" => $result->study,
+                "image_url" => 'https://schol.io/' . $result->school_logo,
+                // "image_url" => 'https://schol.io/panel/assets/images/steps' . $result->section . '.png',
+                "subtitle" => $result->criteria,
+                "buttons" => array(
+                    [
+                        "type" => "web_url",
+                        "url" => "https://schol.io/scholarship/" . $result->scholarship_id,
+                        "title" => "Προβολή",
+                    ],
+                    [
+                        "type" => "web_url",
+                        "url" => 'https://schol.io/public/profile/' . $result->school_id,
+                        "title" => $result->school,
+                    ],
+                ),
+            );
+            if ($index <= 9) {
+                array_push($elements, $array);
+            }
+        }
+
+        $json = [
+            "messages" => [
+                [
+                    "attachment" => [
+                        "type" => "template",
+                        "payload" => [
+                            "template_type" => "generic",
+                            "elements" => $elements,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        return $json;
+    }
+
+    public function schoolCustom()
+    {
+        $results = AlgoliaSchool::search(request()->search)->get();
+
+        if ($results->isEmpty()) {
+            return [
+                "redirect_to_blocks" => ["NotFound"],
+            ];
+        }
+
+        $elements = [];
+
+        foreach ($results as $index => $result) {
+            $array = array(
+                "title" => $result->name,
+                "image_url" => 'https://schol.io' . $result->image,
+                "subtitle" => $result->lengthScholarships . " Υποτροφίες",
+                "buttons" => array(
+                    [
+                        "type" => "web_url",
+                        "url" => "https://schol.io/public/profile/" . $result->id,
+                        "title" => "Προβολή",
+                    ],
+                    [
+                        "type" => "web_url",
+                        "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
+                        "title" => "Υποτροφίες",
+                    ],
+                ),
+            );
+            if ($index <= 9) {
+                array_push($elements, $array);
+            }
+        }
+
+        $json = [
+            "messages" => [
+                [
+                    "attachment" => [
+                        "type" => "template",
+                        "payload" => [
+                            "template_type" => "generic",
+                            "elements" => $elements,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        return $json;
+    }
+
+    
+}
--- app/Scholio/bot-routes.php
+++ app/Scholio/bot-routes.php
@@ -5,289 +5,40 @@ use App\Models\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\School;
 
-Route::get('/app/school/{school}', function(School $school){
-        $levels = [];
-        foreach ($school->type->level as $level) {
-            array_push($levels, $level);
-            foreach ($level->section as $section) {
-                foreach ($section->study as $study) {
-                }
-            }
-        }
-        $data = array(
-            'levels' => $levels,
-        );
-    return [$school->load('admin', 'type', 'image'), $data];
-    // return School::with('admin', 'type')->take($num)->get();
-});
+Route::get('/app/school/{school}', 'BotController@appSchool');
 
-Route::get('/app/schools/{num}', function($num){
-    return School::with('admin', 'type')->take($num)->get();
-});
+Route::get('/app/schools/{num}', 'BotController@appSchoolNum');
 
-Route::get('/app/allSchools', function($num){
-    return School::with('admin', 'type')->get();
-});
+Route::get('/app/allSchools', 'BotController@appAllSchools');
 
-Route::get('/app/{find}', function ($find) {
-    $results = AlgoliaSchool::search($find)->get();
-
-    return $results;
-
-    if ($results->isEmpty()) {
-        return [
-            "redirect_to_blocks" => ["NotFound"],
-        ];
-    }
-
-    $elements = [];
-
-    foreach ($results as $index => $result) {
-        $array = array(
-            "title" => $result->name,
-            "image_url" => 'https://schol.io' . $result->image,
-            "subtitle" => $result->lengthScholarships(),
-            "studies" => $result->lengthStudies(),
-            "buttons" => array(
-                [
-                    "type" => "web_url",
-                    "url" => "https://schol.io/public/profile/" . $result->id,
-                    "title" => "Προβολή",
-                ],
-                [
-                    "type" => "web_url",
-                    "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
-                    "title" => "Υποτροφίες",
-                ],
-            ),
-        );
-        if ($index <= 9) {
-            array_push($elements, $array);
-        }
-    }
-    return $elements;
-});
+Route::get('/app/{find}', 'BotController@appFind');
 
 
-Route::get('/school/search', function () {
-    $results = AlgoliaSchool::search(request()->search)->get();
+Route::get('/school/search', 'BotController@schoolSearch');
 
-    if ($results->isEmpty()) {
-        return [
-            "redirect_to_blocks" => ["NotFound"],
-        ];
-    }
+Route::get('/scholarship/search', 'BotController@scholarshipSearch');
 
-    $elements = [];
+Route::get('/scholarship/custom', 'BotController@scholarshipCustom');
 
-    foreach ($results as $index => $result) {
-        $array = array(
-            "title" => $result->name,
-            "image_url" => 'https://schol.io' . $result->image,
-            "subtitle" => $result->lengthScholarships . " Υποτροφίες",
-            "buttons" => array(
-                [
-                    "type" => "web_url",
-                    "url" => "https://schol.io/public/profile/" . $result->id,
-                    "title" => "Προβολή",
-                ],
-                [
-                    "type" => "web_url",
-                    "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
-                    "title" => "Υποτροφίες",
-                ],
-            ),
-        );
-        if ($index <= 9) {
-            array_push($elements, $array);
-        }
-    }
+Route::get('/school/custom', 'BotController@schoolCustom');
 
-    $json = [
-        "messages" => [
-            [
-                "attachment" => [
-                    "type" => "template",
-                    "payload" => [
-                        "template_type" => "generic",
-                        "elements" => $elements,
-                    ],
-                ],
-            ],
-        ],
-    ];
-    return $json;
-});
+// Route::get('/chat', function () {
+//     $n = [
+//         "type" => "show_block",
+//         "block_name" => "AuthChat",
+//         "title" => "AuthChat",
+//     ];
 
-Route::get('/scholarship/search', function () {
-    $results = AlgoliaScholarship::search(request()->search)->get();
+//     if (request()->name == 'Kostis') {
+//         return [
+//             "redirect_to_blocks" => ["AuthChat"],
+//         ];
+//     }
 
-    if ($results->isEmpty()) {
-        return [
-            "redirect_to_blocks" => ["NotFound"],
-        ];
-    }
-
-    $elements = [];
-
-    foreach ($results as $index => $result) {
-        $array = array(
-            "title" => $result->study,
-            "image_url" => 'https://schol.io/' . $result->school_logo,
-            // "image_url" => 'https://schol.io/panel/assets/images/steps' . $result->section . '.png',
-            "subtitle" => $result->criteria,
-            "buttons" => array(
-                [
-                    "type" => "web_url",
-                    "url" => "https://schol.io/scholarship/" . $result->scholarship_id,
-                    "title" => "Προβολή",
-                ],
-                [
-                    "type" => "web_url",
-                    "url" => 'https://schol.io/public/profile/' . $result->school_id,
-                    "title" => $result->school,
-                ],
-            ),
-        );
-        if ($index <= 9) {
-            array_push($elements, $array);
-        }
-    }
-
-    $json = [
-        "messages" => [
-            [
-                "attachment" => [
-                    "type" => "template",
-                    "payload" => [
-                        "template_type" => "generic",
-                        "elements" => $elements,
-                    ],
-                ],
-            ],
-        ],
-    ];
-    return $json;
-});
-
-Route::get('/scholarship/custom', function () {
-    $results = AlgoliaScholarship::search(request()->search)->get();
-
-    if ($results->isEmpty()) {
-        return [
-            "redirect_to_blocks" => ["NotFound"],
-        ];
-    }
-
-    $elements = [];
-
-    foreach ($results as $index => $result) {
-        $array = array(
-            "title" => $result->study,
-            "image_url" => 'https://schol.io/' . $result->school_logo,
-            // "image_url" => 'https://schol.io/panel/assets/images/steps' . $result->section . '.png',
-            "subtitle" => $result->criteria,
-            "buttons" => array(
-                [
-                    "type" => "web_url",
-                    "url" => "https://schol.io/scholarship/" . $result->scholarship_id,
-                    "title" => "Προβολή",
-                ],
-                [
-                    "type" => "web_url",
-                    "url" => 'https://schol.io/public/profile/' . $result->school_id,
-                    "title" => $result->school,
-                ],
-            ),
-        );
-        if ($index <= 9) {
-            array_push($elements, $array);
-        }
-    }
-
-    $json = [
-        "messages" => [
-            [
-                "attachment" => [
-                    "type" => "template",
-                    "payload" => [
-                        "template_type" => "generic",
-                        "elements" => $elements,
-                    ],
-                ],
-            ],
-        ],
-    ];
-    return $json;
-});
-
-Route::get('/school/custom', function () {
-    $results = AlgoliaSchool::search(request()->search)->get();
-
-    if ($results->isEmpty()) {
-        return [
-            "redirect_to_blocks" => ["NotFound"],
-        ];
-    }
-
-    $elements = [];
-
-    foreach ($results as $index => $result) {
-        $array = array(
-            "title" => $result->name,
-            "image_url" => 'https://schol.io' . $result->image,
-            "subtitle" => $result->lengthScholarships . " Υποτροφίες",
-            "buttons" => array(
-                [
-                    "type" => "web_url",
-                    "url" => "https://schol.io/public/profile/" . $result->id,
-                    "title" => "Προβολή",
-                ],
-                [
-                    "type" => "web_url",
-                    "url" => 'https://schol.io/public/scholarships?q=' . $result->name,
-                    "title" => "Υποτροφίες",
-                ],
-            ),
-        );
-        if ($index <= 9) {
-            array_push($elements, $array);
-        }
-    }
-
-    $json = [
-        "messages" => [
-            [
-                "attachment" => [
-                    "type" => "template",
-                    "payload" => [
-                        "template_type" => "generic",
-                        "elements" => $elements,
-                    ],
-                ],
-            ],
-        ],
-    ];
-    return $json;
-});
-
-Route::get('/chat', function () {
-    $n = [
-        "type" => "show_block",
-        "block_name" => "AuthChat",
-        "title" => "AuthChat",
-    ];
-
-    if (request()->name == 'Kostis') {
-        return [
-            "redirect_to_blocks" => ["AuthChat"],
-        ];
-    }
-
-    $l = [
-        "messages" => [
-            ["text" => "OXI!"],
-        ],
-    ];
-    return $l;
-});
+//     $l = [
+//         "messages" => [
+//             ["text" => "OXI!"],
+//         ],
+//     ];
+//     return $l;
+// });
