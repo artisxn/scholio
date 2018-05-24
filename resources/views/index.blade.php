bdd89a1adef7ceb7508422dda9f6139297d37301
Apostolos Siokas <apostolossiokas@gmail.com>
2018-05-24 13:56:13 +0300

    fb ttest

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
 <html lang="en" class="no-js">
 
 <head>
+		<meta property="fb:app_id" content="1167687200016783" /> 
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://schol.io">
+		<meta property="og:title" content="Schol.io - Still looking for the perfect study?" />
+		<meta property="og:description" content="Στο Schol.io θα βρεις τα δημοφιλέστερα εκπαιδευτικά ιδρύματα!" />
+		<meta property="og:image" content="https://github.com/schol-io/scholio/blob/master/public/new/img/metamini.jpg" />
+		<meta property="og:image:type" content="image/jpg">
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
@@ -10,13 +17,7 @@
 	<meta name="description" content="Στο Schol.io θα βρεις τα δημοφιλέστερα εκπαιδευτικά ιδρύματα!">
 	<meta name="keywords" content="Κολλέγιο, ΙΕΚ, Σχολείο, Δημοτικό, Γυμνάσιο, Λύκειο, Πανεπιστήμιο, ΚΕΚ, ΙΙΕΚ, Σχολές, Σπουδές, Εκπαιδευτικά Ιδρύματα">
 
-	<meta property="fb:app_id" content="1167687200016783" /> 
-	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://schol.io">
-	<meta property="og:title" content="Schol.io - Still looking for the perfect study?" />
-    <meta property="og:description" content="Στο Schol.io θα βρεις τα δημοφιλέστερα εκπαιδευτικά ιδρύματα!" />
-    <meta property="og:image" content="https://github.com/schol-io/scholio/blob/master/public/new/img/metamini.jpg" />
-    <meta property="og:image:type" content="image/jpg">
+	
 
 	@include('google.main')
 	<title>Schol.io - Still looking for the perfect study?</title>
