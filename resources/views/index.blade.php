76db48b6b6c50e8027b60fbbf8c2f70504148673
Apostolos Siokas <apostolossiokas@gmail.com>
2018-06-27 12:24:43 +0300

    google verify

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,8 @@
 		<meta property="og:image" content="https://schol.io/new/img/metamini.jpg" />
 		<meta property="og:image:type" content="image/jpg">
 
+		<meta name="google-site-verification" content="gIvgFOmd0fljlwlSORv5tswdbBGRoT9zU5reco2Ni0c" />
+
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
