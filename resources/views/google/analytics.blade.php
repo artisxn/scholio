5ab3ffc28aca26848ad942cef4f6d9aef409cbab
Apostolos Siokas <apostolossiokas@gmail.com>
2018-07-03 12:53:39 +0300

    local

--- resources/views/google/analytics.blade.php
+++ resources/views/google/analytics.blade.php
@@ -1,4 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
+@if(!App::environment('local'))
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118487911-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -7,3 +8,4 @@
 
   gtag('config', 'UA-118487911-1');
 </script>
+@endif
