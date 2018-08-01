c564044c9f5948ec72c6cbb510249f024bd66073
kfrei <freiderikosk@hotmail.com>
2018-08-01 17:21:30 +0300

    frontistiria5 seeder

--- database/seeds/Portal/Frontistiria4.php
+++ database/seeds/Portal/Frontistiria4.php
@@ -701,39 +701,7 @@ class Frontistiria4 extends Seeder
 
 
 
-        /*=============== 18================*/
 
-//        $fake = factory(App\Models\School::class)->create([
-//            'user_id' => factory(App\User::class)->create(['name' => '', 'email' => '', 'password' => bcrypt('123456'), 'role' => 'school', 'username' => ''])->id,
-//            'address' => '',
-//            'city' => 'Θεσσαλονίκη',
-//            'phone' => 2310,
-//            'type_id' => 3,
-//            'website' => '',
-//            'approved' => 1,
-//            'about' => '
-//
-//            ',
-//            'background' => $background,
-//        ]);
-//
-//        new Portal($fake, 5, 'frontistirio');
-//
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'Α΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'B΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'Γ΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Α΄ Λυκείου', 'Γενική Παιδεία', 'Α΄ Λυκείου');
-//        Scholio::portalStudy($fake, 'B΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Θετικών Σπουδών');
-//        Scholio::portalStudy($fake, 'B΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Ανθρωπιστικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Θετικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Ανθρωπιστικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Οικονομικών Σπουδών & Πληροφορικής');
-//
-////        Scholio::portalSocial($fake, 'facebook', 'https://www.facebook.com/');
-////        Scholio::portalSocial($fake, 'instagram', 'https://www.instagram.com/');
-////        Scholio::portalSocial($fake, 'youtube', 'https://www.youtube.com/');
-//
-//
 
 
 
