ade7b07a2bca386c9f97b95ee11d1d3f2e066347
kfrei <freiderikosk@hotmail.com>
2018-08-12 01:04:30 +0300

    frontistiria7 seeder

--- database/seeds/Portal/Frontistiria7.php
+++ database/seeds/Portal/Frontistiria7.php
@@ -358,7 +358,7 @@ E-learning
      Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Οικονομικών Σπουδών & Πληροφορικής');
 
        Scholio::portalSocial($fake, 'facebook', 'https://www.facebook.com/Φροντιστήρια-Ανοδικό-291573694350542');
-)
+
 
 
      /*=============== 9 Μεθοδικό Κορωπί ================*/
@@ -690,38 +690,6 @@ Online ενημέρωση και Τακτικές ενημερώσεις γον�
 
 
 
-
-        /*=============== 16 ================*/
-
-//        $fake = factory(App\Models\School::class)->create([
-//            'user_id' => factory(App\User::class)->create(['name' => '', 'email' => '', 'password' => bcrypt('123456'), 'role' => 'school', 'username' => ''])->id,
-//            'address' => '',
-//            'city' => 'Αθήνα',
-//            'phone' => 210,
-//            'type_id' => 3,
-//            'website' => '',
-//            'approved' => 1,
-//            'about' => '
-//
-//            ',
-//            'background' => $background,
-//        ]);
-//
-//        new Portal($fake, 5, 'frontistirio');
-//
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'Α΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'B΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Γυμνάσιο', 'Γυμνάσιο', 'Γ΄ Γυμνασίου');
-//        Scholio::portalStudy($fake, 'Α΄ Λυκείου', 'Γενική Παιδεία', 'Α΄ Λυκείου');
-//        Scholio::portalStudy($fake, 'B΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Θετικών Σπουδών');
-//        Scholio::portalStudy($fake, 'B΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Ανθρωπιστικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Θετικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Ανθρωπιστικών Σπουδών');
-//        Scholio::portalStudy($fake, 'Γ΄ Λυκείου', 'Ομάδα Προσανατολισμού', 'Οικονομικών Σπουδών & Πληροφορικής');
-
-//       Scholio::portalSocial($fake, 'facebook', 'https://www.facebook.com/');
-//       Scholio::portalSocial($fake, 'instagram', 'https://www.instagram.com/');
-//       Scholio::portalSocial($fake, 'youtube', 'https://www.youtube.com/')
 
 
     }
