175e310a17af1e471dba0fa5e8d6fdb228e5a77a
kfrei <freiderikosk@hotmail.com>
2018-09-20 04:29:53 +0300

    footer links

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -6,6 +6,9 @@
     .social-link:visited,.social-link:active,.social-link:focus{color: #fff}
     .social-link:hover{color: #FD6A33}
 
+    .sc-hr {  border-top: 1px solid #777; margin-top: 50px; }
+
+
 </style>
 
 
@@ -35,6 +38,56 @@
             </div>
         </div>
 
+
+        <hr class="sc-hr">
+
+        <div class="entered-text margin-bot-50 margin-top-50">
+
+            <div class="row font-weight-300">
+
+                <div class="col-sm-6 col-lg-3 margin-top-20">
+                    <div class="sc-t-white rowTitle margin-bot-3">Κολλέγια</div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray" href="https://schol.io/public/schools?q=%CE%9A%CE%BF%CE%BB%CE%BB%CE%B5%CE%B3%CE%B9%CE%B1%20%CE%91%CE%B8%CE%B7%CE%BD%CE%B1">Κολλεγια Αθηνα</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="https://schol.io/public/schools?q=%CE%9A%CE%BF%CE%BB%CE%BB%CE%AD%CE%B3%CE%B9%CE%B1%20%CE%98%CE%B5%CF%83%CF%83%CE%B1%CE%BB%CE%BF%CE%BD%CE%B9%CE%BA%CE%B7">Κολλεγια Θεσσαλονικη</a></div>
+
+                    <div class="sc-t-white rowTitle margin-top-20 margin-bot-3">Ιδιωτικά Σχολεία</div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray" href="">Ιδιωτικα Σχολεια Αθηνα</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Ιδιωτικα Σχολεια Θεσσαλονικη</a></div>
+                </div>
+
+
+                <div class="col-sm-6 col-lg-3 margin-top-20">
+                    <div class="sc-t-white rowTitle margin-bot-3">I.E.K.</div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray" href="">IEK Αθηνα</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">IEK Θεσσαλονικη</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">IEK Ιωαννινα</a></div>
+                </div>
+
+                <div class="col-sm-6 col-lg-3 margin-top-20">
+                    <div class="sc-t-white rowTitle margin-bot-3">Φροντιστήρια Μέσης Εκπαίδευσης</div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray" href="">Φροντιστηρια Θεσσαλονικη</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Φροντιστηρια Πειραιας</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Φροντιστηρια Μαρουσι</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Φροντιστηρια Περιστερι</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Φροντιστηρια Καλλιθεα</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Φροντιστηρια Ηλιουπολη</a></div>
+
+                </div>
+
+                <div class="col-sm-6 col-lg-3 margin-top-20">
+                    <div class="sc-t-white rowTitle margin-bot-3">Κέντρα Ξένων Γλωσσών</div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray" href="">Ξενες Γλωσσες Θεσσαλονικη</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Ξενες Γλωσσες Πειραιας</a></div>
+                    <div class="margin-bot-3"><a class="sc-t-light-gray"  href="">Ξενες Γλωσσες Ηλιουπολη</a></div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
         <div class=" centered-text margin-bot-50">
             <p class="sc-t-white margin-top-50">@lang('main.footer.message')</p>
             {{--<img height="27px" class="margin-top-20" src="/new/img/laravel-small2.png">--}}
@@ -44,12 +97,7 @@
 
         </div>
 
-        <div class="entered-text margin-bot-50">
-            <p class="sc-t-white margin-top-50">
-                <a href="https://scholio.test/public/schools?q=%CE%9A%CE%BF%CE%BB%CE%BB%CE%AD%CE%B3%CE%B9%CE%B1%20%CE%98%CE%B5%CF%83%CF%83%CE%B1%CE%BB%CE%BF%CE%BD%CE%B9%CE%BA%CE%B7">Κολλεγια Θεσσαλονικη</a><br>
-                <a href="https://scholio.test/public/schools?q=%CE%9A%CE%BF%CE%BB%CE%BB%CE%B5%CE%B3%CE%B9%CE%B1%20%CE%91%CE%B8%CE%B7%CE%BD%CE%B1">Κολλεγια Αθηνα</a><br>
-            </p>
-        </div>
+
     </div>
 </footer>
 
