c807256394103c1d8a6447fd45cd6c493c73a78c
Apostolos Siokas <apostolossiokas@gmail.com>
2018-09-26 19:35:27 +0300

    adding region

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,11 @@ Route::get('aaqq', function(){
         $alg = AlgoliaSchool::where('school_id', $school->id)->first();
             $alg->region = $school->region;
             $alg->save();
+
+            $alg->{'categories.lvl0'} = $school->city;
+            $alg->{'categories.lvl1'} = $school->city .  '>' . $school->region;
+            $alg->searchable();
     }
-
-
 });
 
 Route::get('/saveStudyLink', function () {
