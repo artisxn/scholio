dbb2f3373f5543e2cfe08f7e460a5511829d23e2
Apostolos Siokas <apostolossiokas@gmail.com>
2018-10-18 15:10:03 +0300

    change xenes glwses profile studies

--- resources/views/public/school/profile.blade.php
+++ resources/views/public/school/profile.blade.php
@@ -29,21 +29,13 @@
 <!-- Favicon -->
 <link rel="shortcut icon" href="{{asset('new/img/test-black.png')}}" type="image/x-icon" />
 
-<!-- Google Fonts
-<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&subset=greek" rel="stylesheet"> -->
 
 <!-- Bootstrap -->
 <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 
-<!-- Bootstrap Select -->
-<!-- <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css" rel="stylesheet" > -->
-
 <!-- font-awesome -->
 <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
 
-
-<!-- Bootstrap xxs Extension CSS -->
-<!-- <link href="{{asset('new/css/Bootstrap-xxs-xxxs.css')}}" rel="stylesheet"> -->
 
 <!-- Main CSS -->
 <link href="{{asset('new/css/main.css')}}" rel="stylesheet">
@@ -55,7 +47,7 @@
     <link href="/new/css/profile.css" rel="stylesheet">
 
     <!-- Horizontal Ribbons  CSS -->
-    <!-- <link href="/new/css/ribbonHorizontal.css" rel="stylesheet"> -->
+    <link href="/new/css/ribbonHorizontal.css" rel="stylesheet">
 
     <!-- Hexagon CSS -->
     <link href="/new/css/Hexagon.css" rel="stylesheet">
@@ -68,8 +60,6 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
-
-    @include('public.window-lang')
 
     <style>
         .schoolname{
