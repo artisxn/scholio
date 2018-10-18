45298de4f3e8927fd32f61fcc398e60692a8b60e
Apostolos Siokas <apostolossiokas@gmail.com>
2018-10-18 12:52:53 +0300

    mobile friendly check --supercheck

--- resources/views/public/school/profile.blade.php
+++ resources/views/public/school/profile.blade.php
@@ -25,7 +25,28 @@
 
 
     <!--====== CSS  Styles =======-->
-    @include('public.styles')
+    
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{asset('new/img/test-black.png')}}" type="image/x-icon" />
+
+<!-- Google Fonts
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&subset=greek" rel="stylesheet"> -->
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+
+<!-- Bootstrap Select -->
+<!-- <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css" rel="stylesheet" > -->
+
+<!-- font-awesome -->
+<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+
+
+<!-- Bootstrap xxs Extension CSS -->
+<link href="{{asset('new/css/Bootstrap-xxs-xxxs.css')}}" rel="stylesheet">
+
+<!-- Main CSS -->
+<link href="{{asset('new/css/main.css')}}" rel="stylesheet">
 
     <!-- BX Slider CSS -->
     <link href="/new/css/jquery.bxslider.css" rel="preload" as="style" onload="this.rel='stylesheet'">
