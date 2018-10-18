0e43a3f964a87b36a779f8bcd6623bc01a7c5d58
Apostolos Siokas <apostolossiokas@gmail.com>
2018-10-18 12:50:50 +0300

    mobile friendly check --supercheck

--- resources/views/public/styles.blade.php
+++ resources/views/public/styles.blade.php
@@ -7,17 +7,17 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&subset=greek" rel="stylesheet"> -->
 
 <!-- Bootstrap -->
-<link rel="preload" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" as="style" onload="this.rel='stylesheet'">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 
 <!-- Bootstrap Select -->
-<link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css" rel="preload"  as="style" onload="this.rel='stylesheet'">
+<!-- <link type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css" rel="stylesheet" > -->
 
 <!-- font-awesome -->
-<link rel="preload" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" as="style" onload="this.rel='stylesheet'">
+<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
 
 
 <!-- Bootstrap xxs Extension CSS -->
-<link href="{{asset('new/css/Bootstrap-xxs-xxxs.css')}}" rel="preload" as="style" onload="this.rel='stylesheet'">
+<link href="{{asset('new/css/Bootstrap-xxs-xxxs.css')}}" rel="stylesheet">
 
 <!-- Main CSS -->
-<link href="{{asset('new/css/main.css')}}" rel="preload" as="style" onload="this.rel='stylesheet'">
+<link href="{{asset('new/css/main.css')}}" rel="stylesheet">
